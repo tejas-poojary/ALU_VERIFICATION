93972cba5ab5b292a0ee14c11e05ec0479eef4a3
tejas-poojary <tejaspoojary2003@gmail.com>
2025-07-31 02:49:14 +0530

    Create alu_package.sv

---
+++ codes/alu_package.sv
@@ -0,0 +1,12 @@
+package alu_pkg;
+
+`include "alu_transaction.sv"
+`include "alu_generator.sv"
+`include "alu_driver_new.sv"
+`include "alu_monitor.sv"
+`include "reference_model_version1.sv"
+`include "alu_scoreboard.sv"
+`include "alu_environment.sv"
+`include "alu_test.sv"
+
+endpackage
